dd7e6ca3145e40fde36a95f52763bf88a0deb80a
jun9898 <nice1998@gmail.com>
2024-12-23 12:13:09 +0900

    project init

---
+++ src/main/java/boongeo/halftime/HalftimeApplication.java
@@ -0,0 +1,13 @@
+package boongeo.halftime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalftimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalftimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boongeo/halftime/HalftimeApplicationTests.java
@@ -0,0 +1,13 @@
+package boongeo.halftime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalftimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
